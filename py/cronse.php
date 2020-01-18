ed354e706f53ebaa15494c6b940d66e77112bb74
tzuhunglin <alfredlin417@gmail.com>
2020-01-18 19:56:40 +0800

    cronse modified

--- py/cronse.php
+++ py/cronse.php
@@ -3,25 +3,13 @@ date_default_timezone_set("Asia/Taipei");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $bLoop = true;
-do
+
+if(date("H:i")>="10:30" && date("H:i")<="21:35")
 {
-	if(date("H:i")>="10:30" && date("H:i")<="21:35")
+	if(date("i")=="34"||date("i")=="04")
 	{
-		if(date("i")=="34"||date("i")=="04")
-		{
-			echo "<pre>"; print_r(date("H:i"));
-	        exec('python /var/www/html/boyigame/py/se.py');
-
-			sleep(60);
-		}
-		sleep(30);
-
+        exec('python /var/www/html/boyigame/py/se.py');
 	}
-	else
-	{
-		sleep(60);
-	}
-
 }
-while($bLoop);
+
 ?>
