ae14eff5f1af0af39d881d6aa8be0b622352d8c4
tzuhunglin <alfredlin417@gmail.com>
2020-01-19 16:13:37 +0800

    add cronsocket

---
+++ py/cronsocket.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$connection = @fsockopen('localhost', '3000');
+
+if (is_resource($connection))
+{
+    exec('root node /var/www/html/boyigame/socket.js');
+}
+?>
