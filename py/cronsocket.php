b5c854d91075aee0e15ee21abd8c3cc6c7490a71
tzuhunglin <alfredlin417@gmail.com>
2020-01-19 16:23:43 +0800

    condition modified

--- py/cronsocket.php
+++ py/cronsocket.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $connection = @fsockopen('localhost', '3000');
 
-if (is_resource($connection))
+if (!is_resource($connection))
 {
     exec('root node /var/www/html/boyigame/socket.js');
 }
